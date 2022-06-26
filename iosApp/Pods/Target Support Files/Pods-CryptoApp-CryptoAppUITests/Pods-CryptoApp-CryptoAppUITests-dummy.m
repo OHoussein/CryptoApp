1f9d06345c4f40539faeb4fe66b06a3c1908e6b5
OHoussein <w.h.ouerg@gmail.com>
2022-06-26 23:21:54 +0200

    [ARCH] common gradle config

---
+++ iosApp/Pods/Target Support Files/Pods-CryptoApp-CryptoAppUITests/Pods-CryptoApp-CryptoAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CryptoApp_CryptoAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CryptoApp_CryptoAppUITests
+@end
---
+++ iosApp/Pods/Target Support Files/Pods-CryptoApp/Pods-CryptoApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CryptoApp : NSObject
+@end
+@implementation PodsDummy_Pods_CryptoApp
+@end

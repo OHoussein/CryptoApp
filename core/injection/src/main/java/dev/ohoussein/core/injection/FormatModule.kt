fb9a333cfd3ee2b2d571a739ab362a46ee165e02
OHoussein <w.h.ouerg@gmail.com>
2022-07-13 16:17:07 +0200

    [ARCH] remove unnecessary detekt plugin

--- core/injection/src/main/java/dev/ohoussein/core/injection/FormatModule.kt
+++ core/injection/src/main/java/dev/ohoussein/core/injection/FormatModule.kt
@@ -6,6 +6,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dev.ohoussein.cryptoapp.presentation.mapper.PercentMapper
 import dev.ohoussein.cryptoapp.presentation.mapper.PriceMapper
+import java.util.Locale
 
 @Module
 @InstallIn(SingletonComponent::class)

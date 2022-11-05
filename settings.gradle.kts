712172ac1625d384eb197e03b1142c75da8b9768
OHoussein <w.h.ouerg@gmail.com>
2022-11-05 20:58:51 +0100

    [GRADLE] finish kts migration

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+rootProject.name = "cryptoapp"
+
+include(":app")
+include(":crypto:data")
+include(":crypto:domain")
+include(":crypto:presentation")
+include(":core:test")
+include(":core:common")
+include(":core:injection")
+include(":core:designsystem")
+include(":core:formatter")
+include(":data:network")
+include(":data:database")
+include(":core:injection:core")
+include(":core:cached-data")
+include(":data:cache")

4456b1307381adb6c2dbdd5da846a6841d74bf41
OHoussein <w.h.ouerg@gmail.com>
2022-11-08 18:03:57 +0100

    [DEPS] Update deps

--- build-logic/convention/src/main/kotlin/dev/ohoussein/cryptoapp/SdkVersion.kt
+++ build-logic/convention/src/main/kotlin/dev/ohoussein/cryptoapp/SdkVersion.kt
@@ -2,6 +2,6 @@ package dev.ohoussein.cryptoapp
 
 object SdkVersion {
     const val MIN_SDK_VERSION = 23
-    const val COMPILE_SDK_VERSION = 32
-    const val TARGET_SDK_VERSION = 32
+    const val COMPILE_SDK_VERSION = 33
+    const val TARGET_SDK_VERSION = 33
 }

e66782e94c1c26d813ac67287a34af5a56019883
HOUSSEIN <houssein.ouerghemmi@happn.fr>
2024-05-28 15:24:55 +0200

    [CMP] Update kotlin and compose plugin

--- app-desktop/build.gradle.kts
+++ app-desktop/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
     kotlin("jvm")
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
 }
 
 group = "dev.ohoussein.cryptoapp"

b527d2e39a31c43f28eba61cf0627cd4ccd1a195
HOUSSEIN <houssein.ouerghemmi@happn.fr>
2024-06-03 22:22:41 +0200

    [ANDROID] Fix android app

--- android/app/src/main/java/dev/ohoussein/cryptoapp/MainActivity.kt
+++ android/app/src/main/java/dev/ohoussein/cryptoapp/MainActivity.kt
@@ -2,7 +2,7 @@ package dev.ohoussein.cryptoapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.ohoussein.tldr.createApp
+import dev.ohoussein.cryptoapp.dev.ohoussein.cryptoapp.createApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ shared/presentation/src/androidMain/kotlin/dev/ohoussein/cryptoapp/AppFactory.kt
@@ -0,0 +1,19 @@
+package dev.ohoussein.cryptoapp.dev.ohoussein.cryptoapp
+
+import SharedApp
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+fun ComponentActivity.createApp() {
+    setContent {
+        SharedApp()
+    }
+}
+
+@Preview
+@Composable
+internal fun PreviewApp() {
+    SharedApp()
+}

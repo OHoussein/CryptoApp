80bb20ed5ac1c14408fdc1742b772b1fbe4677fc
HOUSSEIN <houssein.ouerghemmi@happn.fr>
2024-06-03 22:10:10 +0200

    [BUILD] Update the CI

--- e2e_tests/test_ios.sh
+++ e2e_tests/test_ios.sh
@@ -1,8 +1,9 @@
-xcodebuild -scheme CryptoAppiOS -workspace ../CryptoAppiOS/CryptoAppiOS.xcworkspace \
- CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO \
- -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.1' \
- -derivedDataPath \
- build
+xcodebuild \
+          -workspace ../app-iOS/appiOS.xcodeproj/project.xcworkspace \
+          -configuration Release \
+          -scheme appiOS \
+          -sdk iphonesimulator \
+          -derivedDataPath app-iOS/build
 
-xcrun simctl install Booted ../CryptoAppiOS/build/Build/Products/Debug-iphonesimulator/CryptoAppiOS.app
+xcrun simctl install Booted ../app-iOS/build/Build/Products/Debug-iphonesimulator/CryptoAppiOS.app
 maestro test ios/ios.yml

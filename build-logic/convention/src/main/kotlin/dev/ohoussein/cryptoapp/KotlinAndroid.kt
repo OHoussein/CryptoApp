5973c82a9166a7d28bab4f52751759fc7bdfe243
OHoussein <ouerghemmi.houssein@gmail.com>
2025-02-05 10:55:49 +0100

    [CORE] Fix Version bump

--- build-logic/convention/src/main/kotlin/dev/ohoussein/cryptoapp/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/dev/ohoussein/cryptoapp/KotlinAndroid.kt
@@ -8,7 +8,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 internal fun Project.configureKotlinAndroid(
-    commonExtension: CommonExtension<*, *, *, *, *>,
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
 ) = commonExtension.apply {
 
     compileSdk = SdkVersion.COMPILE_SDK_VERSION

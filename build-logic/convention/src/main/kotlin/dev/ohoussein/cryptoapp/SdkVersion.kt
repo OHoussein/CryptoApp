dc49b3dda89318ac74a46404c4fbd246512c095b
OHoussein <ouerghemmi.houssein@gmail.com>
2025-02-05 10:55:24 +0100

    [ANDROID] Bump target and compile SDK

--- build-logic/convention/src/main/kotlin/dev/ohoussein/cryptoapp/SdkVersion.kt
+++ build-logic/convention/src/main/kotlin/dev/ohoussein/cryptoapp/SdkVersion.kt
@@ -2,6 +2,6 @@ package dev.ohoussein.cryptoapp
 
 object SdkVersion {
     const val MIN_SDK_VERSION = 23
-    const val COMPILE_SDK_VERSION = 34
-    const val TARGET_SDK_VERSION = 34
+    const val COMPILE_SDK_VERSION = 35
+    const val TARGET_SDK_VERSION = 35
 }

08140666ebfa8ab4113aa0fad96755bca4b08f1a
Houssein <houssein.ouerghemmi@team.bumble.com>
2026-02-24 13:48:35 +0100

    Fix compilation issues
    # Conflicts:
    #	gradle/libs.versions.toml

--- build-logic/convention/src/main/kotlin/KotlinMultiplatformLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/KotlinMultiplatformLibraryConventionPlugin.kt
@@ -1,4 +1,7 @@
-import com.android.build.gradle.LibraryExtension
+import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryTarget
+import com.android.build.api.dsl.LibraryExtension
+import com.android.build.api.dsl.androidLibrary
+import dev.ohoussein.cryptoapp.SdkVersion
 import dev.ohoussein.cryptoapp.getAndroidNameSpaceFromPath
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -6,6 +9,7 @@ import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeSimulatorTest
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -18,27 +22,56 @@ class KotlinMultiplatformLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+            val warningsAsErrors = findProperty("warningsAsErrors") as? String
 
             with(pluginManager) {
-                apply("com.android.library")
+                apply("com.android.kotlin.multiplatform.library")
                 apply("org.jetbrains.kotlin.multiplatform")
                 apply("dev.ohoussein.cryptoapp.kotlin.detekt")
             }
 
             extensions.configure<KotlinMultiplatformExtension> {
-                androidTarget()
+                // targets.withType<KotlinMultiplatformAndroidLibraryTarget>().configureEach {
+                //     namespace = getAndroidNameSpaceFromPath(PACKAGE, target.path)
+                //     compileSdk = SdkVersion.COMPILE_SDK_VERSION
+                //     minSdk = SdkVersion.MIN_SDK_VERSION
+                //     compilerOptions {
+                //         allWarningsAsErrors.set(warningsAsErrors.toBoolean())
+                //         jvmTarget.set(JvmTarget.JVM_20)
+                //         freeCompilerArgs.addAll(
+                //             "-Xexpect-actual-classes",
+                //             "-opt-in=kotlin.RequiresOptIn",
+                //             "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                //         )
+                //     }
+                // }
+
+                androidLibrary {
+                    namespace = getAndroidNameSpaceFromPath(PACKAGE, target.path)
+                    compileSdk = SdkVersion.COMPILE_SDK_VERSION
+                    minSdk = SdkVersion.MIN_SDK_VERSION
+                }
+
                 iosArm64()
                 iosX64()
                 iosSimulatorArm64()
                 jvm("desktop")
 
                 applyDefaultHierarchyTemplate()
+                compilerOptions {
+                    allWarningsAsErrors.set(warningsAsErrors.toBoolean())
+                    freeCompilerArgs.addAll(
+                        "-Xexpect-actual-classes",
+                        "-opt-in=kotlin.RequiresOptIn",
+                        "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                    )
+                }
             }
 
-            extensions.configure<LibraryExtension> {
-                namespace = getAndroidNameSpaceFromPath(PACKAGE, path)
-                configureKotlinAndroid(this)
-            }
+            // extensions.configure<LibraryExtension> {
+            //     namespace = getAndroidNameSpaceFromPath(PACKAGE, path)
+            //     configureKotlinAndroid(this)
+            // }
 
             extensions.configure<KotlinMultiplatformExtension> {
                 sourceSets.getByName("commonTest").dependencies {
